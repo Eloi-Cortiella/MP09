1000a2d75c9031cea800d1567642cab410adc893
Elvoltios <eloicortiella@iesebre.com>
2023-10-23 18:56:26 +0200

    Sync

--- app/index.php
+++ app/index.php
@@ -1,1 +1,3 @@
 <?php
+
+$greeting = "Hello World!";
---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Página Web</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<header>
+    <h1><?=$greeting;?></h1>
+    <nav>
+    </nav>
+</header>
+
+<section id="contenido">
+    <article>
+        <h2>Artículo 1</h2>
+        <p>Este es el primer artículo de mi página web.</p>
+    </article>
+    <article>
+        <h2>Artículo 2</h2>
+        <p>Este es el segundo artículo de mi página web.</p>
+    </article>
+</section>
+
+<aside>
+    <h3>Barra lateral</h3>
+    <ul>
+        <li>Enlaces útiles</li>
+        <li>Publicidad</li>
+    </ul>
+</aside>
+
+<footer>
+    <p>Derechos de autor © 2023 Mi Página Web</p>
+</footer>
+</body>
+</html>
